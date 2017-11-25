8831b8c68ba1e995f6fd889e7b2fdc45f77168b7
Evan Young <evanyoung62@icloud.com>
2017-11-25 14:49:03 -0600

    Initial commit

---
+++ spec/language-ebhan-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageEbhanView from '../lib/language-ebhan-view';
+
+describe('LanguageEbhanView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
